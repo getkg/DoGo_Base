b3fefcc91c05faa1fdc71f4bdac2f45c6659f2f7
Install <24478744+swsv@users.noreply.github.com>
2021-02-22 15:57:46 +0800

    Update git_pull.sh

--- git_pull.sh
+++ git_pull.sh
@@ -29,7 +29,8 @@ ContentDropTask=${ShellDir}/drop_task
 SendCount=${ShellDir}/send_count
 isTermux=${ANDROID_RUNTIME_ROOT}${ANDROID_ROOT}
 Scripts2URL=https://github.com/shylocks/Loon
-ScriptsURL=https://gitee.com/lxk0301/jd_scripts
+#ScriptsURL=https://gitee.com/lxk0301/jd_scripts
+ScriptsURL=https://hub.docker.com/r/lxk0301/jd_scripts
 ShellURL=https://github.com/getkg/DoGo_Base
   
 
